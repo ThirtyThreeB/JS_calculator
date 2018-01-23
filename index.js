1c15bb1b1f397333b9d4df912aaae00a75375ef5
Gabriel Aragon <thirtythreebananas@gmail.com>
2018-01-23 14:50:01 -0500

    Decimals and duplicate operators

--- index.js
+++ index.js
@@ -5,33 +5,62 @@
 //use loop to asign eventhandler to all buttons
 let memArray = [];
 //let displayStr = memArray.join();
-let isDecimal = false;
+
 
 let btnClick = (e) => {
 	let justClicked = e.target.innerText;
-  //console.log(justClicked);
-  if (justClicked.match(/[.0-9-\+\*\/]/g) && isDecimal === false){
-  	if(justClicked === "."){
-  		isDecimal = true;	
-  		memArray.push(justClicked, "a");
-  	}
-  	else
-  		memArray.push(justClicked, "b");
-  }
-  else if (justClicked.match(/[0-9-\+\*\/]/g)){
-  	memArray.push(justClicked, "c");
-  }
-    //memArray.push(justClicked);
 
-    console.log("memArray=",memArray.join(''), "isDecimal=", isDecimal);
-    document.getElementById("display").innerHTML = memArray.join('');
-  
-  //else { console.log(isNaN(justClicked));
-//}
+console.log(memArray.length);
+	
+	if(justClicked.match(/[.]/)){
+		document.querySelector('#decimal').removeEventListener('click', btnClick);
+		memArray.push(justClicked);
+
+	}
+	else if (justClicked.match(/[-\+\*\/]/)) { 
+		disableOps();
+		memArray.push(justClicked);
+	}
+
+	else {
+		//enableOps();
+		memArray.push(justClicked);
+		enableOps();
+	// 	if (memArray.includes(".") === false){
+	// 	document.querySelector('#decimal').addEventListener('click', btnClick);
+	// }
+	}
+	console.log("memArray=",memArray.join(''));
+	document.getElementById("display").innerHTML = memArray.join('');
 };
 
-const keypad = document.querySelector('.buttons');
-keypad.addEventListener('click', btnClick);
+const keypad = document.getElementsByClassName('digit');
+
+for(var i = 0; i < keypad.length; i++) {
+	keypad[i].addEventListener("click", btnClick);
+}
+
+const operators = document.getElementsByClassName('operator');
+
+let disableOps = () => {
+	for(var i = 0; i < operators.length; i++) {
+		operators[i].removeEventListener("click", btnClick);
+		
+ //operators.forEach(function)
+}
+console.log("disabled");
+};
+
+let enableOps = () => {
+	for(var i = 0; i < operators.length; i++) {
+		operators[i].addEventListener("click", btnClick);
+ //operators.forEach(function)
+}
+console.log("enabled");
+};
+
+
+
 
 //display values in array as string in display
 
